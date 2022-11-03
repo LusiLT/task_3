8c0770f29531ca034881510b3fa6e0aca6375966
LusiLT <101464618+LusiLT@users.noreply.github.com>
2022-11-03 21:59:17 +0700

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,12 @@
+const judul = document.getElementById('judul');
+judul.innerHTML = 'Pemrograman Web'
+
+const sectionb = document.querySelector('section#b')
+sectionb.innerHTML = 'nama saya Lusi Lesmana Tamba';
+
+
+function Event(){
+    p2.style.backgroundColor = 'green';
+}
+const p2 = document.querySelector('.p2');
+p2.onclick = Event;
